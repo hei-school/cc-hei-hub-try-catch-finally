a3e41cf9616f0ff403de240831da63db033355af
Yume <hei.hajatiana@gmail.com>
2023-12-13 16:36:29 +0300

    chore: add write_file_in_dir for saving file in STORAGE

---
+++ src/core/fs/write_file_in_dir.ts
@@ -0,0 +1,23 @@
+import fs from "fs";
+import {FileLockError} from "../error";
+import path from "path";
+
+export const writeFileInDir = async (
+  buf: ArrayBuffer,
+  directory: string,
+  filename: string
+) => {
+  try {
+    const to = getRelativePath(directory, filename);
+    fs.writeFileSync(to, Buffer.from(buf));
+  } catch (e) {
+    console.log("em", e.message);
+    throw new FileLockError();
+  }
+};
+
+const STORAGE = "storage";
+
+const getRelativePath = (directory: string, file: string) => {
+  return path.join(STORAGE, directory, file);
+};
