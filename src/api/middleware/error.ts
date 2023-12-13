d5a9aed01671af8c6b64dd9f1cc5105d5b9f6fc2
Yume <hei.hajatiana@gmail.com>
2023-12-13 18:29:54 +0300

    chore: intercept entity.too.large => common http error

--- src/api/middleware/error.ts
+++ src/api/middleware/error.ts
@@ -1,13 +1,20 @@
-import {HttpError} from "http-errors";
-import {ErrorRequestHandler} from "express";
+import {ErrorRequestHandler, Response} from "express";
 import {CustomError} from "../../core/error/custom_error";
+import {FileSizeExceedError} from "../../core/error";
+import {MAX_FILE_SIZE_IN_KB} from "../../core/constants";
 
-export const errorHandler: ErrorRequestHandler = (
-  err: CustomError,
-  _req,
-  res,
-  next
-) => {
+export const errorHandler: ErrorRequestHandler = (err, _req, res, next) => {
+  if (err instanceof CustomError) {
+    return customErrorToPrettyHttpError(res, err);
+  }
+  // bodyParser
+  if (err.type === "entity.too.large") {
+    const fileSizeExceed = new FileSizeExceedError(MAX_FILE_SIZE_IN_KB);
+    return customErrorToPrettyHttpError(res, fileSizeExceed);
+  }
+};
+
+const customErrorToPrettyHttpError = (res: Response, err: CustomError) => {
   const http = err.toHttp();
   return res.status(http.status).json({
     status: http.status,
