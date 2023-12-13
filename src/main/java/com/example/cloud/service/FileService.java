4fe553a6c66ce25e28f393fc88c38efc2caee788
Haritiana Adriano <hei.adriano.4@gmail.com>
2023-12-13 10:44:16 +0300

    chore: add duplicated checker before inserting file

--- src/main/java/com/example/cloud/service/FileService.java
+++ src/main/java/com/example/cloud/service/FileService.java
@@ -18,14 +18,15 @@ import org.springframework.web.multipart.MultipartFile;
 public class FileService {
   private final FileUtils utils;
 
-
-  public String uploadFile(MultipartFile multipartFile, String path, String filename)
+  public String uploadFile(MultipartFile multipartFile, String dir, String filename)
       throws Exception {
     boolean response = false;
     try {
+      utils.checkDuplicatedFile(dir, filename);
+
       Files.copy(
           multipartFile.getInputStream(),
-          Paths.get(utils.getUploadDirectory(path) + File.separator + filename),
+          Paths.get(utils.getUploadDirectory(dir) + File.separator + filename),
           StandardCopyOption.REPLACE_EXISTING);
       response = true;
     }
